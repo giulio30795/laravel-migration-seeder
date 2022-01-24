328db41f31a4d8f4b51c7f59d8a02ca5d7e1b769
giulio30795 <giulioincarbone95@gmail.com>
2022-01-24 14:51:50 +0100

    setup columns

---
+++ database/migrations/2022_01_24_133050_create_packages_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePackagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('packages', function (Blueprint $table) {
+            $table->id();
+            $table->string('Destination', 100);
+            $table->string('State', 100);
+            $table->string('Kind of Holiday', 50);
+            $table->smallInteger('Days')->unsigned();
+            $table->smallInteger('Price')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('packages');
+    }
+}
